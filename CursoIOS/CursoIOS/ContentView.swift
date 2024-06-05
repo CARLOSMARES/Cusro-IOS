5883e27ae850cac3370bd94433c01415a9c9deab
CARLOSMARES <microsystemags@hotmail.com>
2024-06-04 23:59:48 -0600

    Se crea el ejercicio numero 4 creando una interfaz

--- CursoIOS/CursoIOS/ContentView.swift
+++ CursoIOS/CursoIOS/ContentView.swift
@@ -7,18 +7,53 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Ejercicio4: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            HStack{
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+            }
+            Rectangle()
+                .foregroundColor(.orange)
+                .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+            HStack{
+                Circle()
+                    .foregroundColor(.green)
+                    .frame(height: 150)
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/ ,height: 200)
+                Circle()
+                    .foregroundColor(.blue)
+                    .frame(height: 150)
+            }
+            Rectangle()
+                .foregroundColor(.orange)
+                .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+            HStack{
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+            }
         }
-        .padding()
+        .background(.red)
     }
 }
 
 #Preview {
-    ContentView()
+    Ejercicio4()
 }
