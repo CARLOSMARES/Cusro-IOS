72ed96ec2e550a976696a47f0d471d67257369c3
CARLOSMARES <microsystemags@hotmail.com>
2024-06-23 16:59:22 -0600

    New Modifies

--- CursoIOS/CursoIOS/Components/ImageExample.swift
+++ CursoIOS/CursoIOS/Components/ImageExample.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ImageExample: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("SwiftUI")
+            .resizable()
+            .frame(width: 50, height: 50)
     }
 }
 
